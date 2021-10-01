d2e9c9b3d024fc3e52bd951f9b46e7733ba8a061
Erik Rasmussen <ecr.referee@hotmail.com>
2021-10-01 12:42:49 -0500

    Adding new projects

---
+++ src/ProxmoxJumpProxy.Daemon/Program.cs
@@ -0,0 +1,10 @@
+using ProxmoxJumpProxy.Daemon;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
