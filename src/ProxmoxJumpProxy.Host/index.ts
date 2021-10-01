8a2fd414df9b8d936f453def1827cdfe42493fd0
Erik Rasmussen <ecr.referee@hotmail.com>
2021-10-01 18:26:42 -0500

    WIP on the infra stuff

--- src/ProxmoxJumpProxy.Host/index.ts
+++ src/ProxmoxJumpProxy.Host/index.ts
@@ -1,23 +1,56 @@
 import * as pulumi from '@pulumi/pulumi';
+import * as web from '@pulumi/azure-native/web';
 import * as resources from '@pulumi/azure-native/resources';
+import * as signalr from '@pulumi/azure-native/signalrservice';
 import * as storage from '@pulumi/azure-native/storage';
+import { pascalCase } from 'pascal-case';
 
-// Create an Azure Resource Group
-const resourceGroup = new resources.ResourceGroup('resourceGroup');
+const stack = pulumi.getStack();
+const stackUpper = pascalCase(stack);
+const isProd = stack === 'prod';
 
-// Create an Azure resource (Storage Account)
-const storageAccount = new storage.StorageAccount('sa', {
+const resourceGroup = new resources.ResourceGroup('resourceGroup', {
+  resourceGroupName: `ProxmoxJumpProxy${isProd ? '' : `-${stackUpper}`}`,
+});
+
+const hubs = new signalr.SignalR('signalr', {
+  resourceName: `unmango-proxmox-signalr${isProd ? '' : `-${stack}`}`,
   resourceGroupName: resourceGroup.name,
+  sku: {
+    name: 'Free_F1',
+    capacity: 1,
+  },
+  kind: signalr.ServiceKind.SignalR,
+  features: [
+    {
+      flag: signalr.FeatureFlags.ServiceMode,
+      value: 'Serverless',
+    },
+  ],
+});
+
+const storageAccount = new storage.StorageAccount('functionsa', {
+  resourceGroupName: resourceGroup.name,
+  kind: storage.Kind.StorageV2,
   sku: {
     name: storage.SkuName.Standard_LRS,
   },
-  kind: storage.Kind.StorageV2,
 });
 
-// Export the primary key of the Storage Account
-const storageAccountKeys = pulumi
-  .all([resourceGroup.name, storageAccount.name])
-  .apply(([resourceGroupName, accountName]) =>
-    storage.listStorageAccountKeys({ resourceGroupName, accountName })
-  );
-export const primaryStorageKey = storageAccountKeys.keys[0].value;
+const plan = new web.AppServicePlan('functions', {
+  name: 'functions-asp',
+  resourceGroupName: resourceGroup.name,
+  sku: {
+    name: 'Y1',
+    tier: 'Dynamic',
+  },
+});
+
+const app = new web.WebApp('functions', {
+  name: `unmango-proxmox-functions${isProd ? '' : `-${stack}`}`,
+  resourceGroupName: resourceGroup.name,
+  serverFarmId: plan.id,
+  kind: 'FunctionApp',
+});
+
+export const hostname = hubs.hostName;
